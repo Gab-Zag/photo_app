0779c00835fd28cd5cbbfb20e9719899c2886cf3
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2025-04-05 19:23:15 -0300

    Refactor HomePage to use Bloc for state management and update UI accordingly

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
+import 'package:photo_app/utils/ui_card.dart';
+
+import '../../domain/entities/photo.dart';
+import 'cubit/photo_cubit.dart';
+import 'cubit/photo_state.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,32 +15,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool checkValue = false;
+  late PhotoCubit _cubit;
+
+  @override
+  void initState() {
+    _cubit = PhotoCubit()..getPhotos();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Photo App'),
-        backgroundColor: Colors.amber,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Checkbox(
-                  value: checkValue,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      checkValue = value ?? true;
-                    });
-                  },
-                ),
-                Text('Texte 1')
-              ],
-            ),
-          ],
-        ),
+        title: const Text('teste'),),
+      body: BlocBuilder<PhotoCubit, PhotoState>(
+        bloc: _cubit,
+        builder: (_, state) {
+          if (state.photos.isEmpty && state.isLoading == false) {
+            return Text('Fotos nao encontradas');
+          }
+          if (state.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return ListView.separated(
+            itemCount: state.photos.length,
+            separatorBuilder: (_, index) => const Gap(10),
+            itemBuilder: (_, index) {
+              Photo photo = state.photos[index];
+              return UiCard(
+                title: photo.title ?? '',
+                url: photo.url ?? '',
+              );
+            },
+          );
+        },
       ),
     );
   }
