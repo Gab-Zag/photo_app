7df57514446e1ef5f7adeddfd3f1b77af3e1f695
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2025-04-06 05:43:37 -0300

    Add CounterState class for managing counter value in Bloc

---
+++ lib/pages/home_page/cubit/couter_state.dart
@@ -0,0 +1,23 @@
+
+import 'package:equatable/equatable.dart';
+
+class CounterState extends Equatable {
+  final int? counter;
+  
+  const CounterState({
+    this.counter = 0,
+  });
+
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        counter,
+      ];
+}
