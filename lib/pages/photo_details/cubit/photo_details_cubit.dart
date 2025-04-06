b6a2be598b5f6a693f7c99d03705e8dbdddee330
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2025-04-06 18:30:30 -0300

    Refactor import statements in PhotoDetailsCubit for consistency

--- lib/pages/photo_details/cubit/photo_details_cubit.dart
+++ lib/pages/photo_details/cubit/photo_details_cubit.dart
@@ -1,11 +1,11 @@
-
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:photo_app/data/dio/repository.dart';
-import 'package:photo_app/pages/photo_details/cubit/photo_details_state.dart';
+import '../../../data/dio/repository.dart';
+import 'photo_details_state.dart';
 
 import '../../../domain/entities/photo.dart';
 
 class PhotoDetailsCubit extends Cubit<PhotoDetailsState>{
+
   PhotoDetailsCubit() : super(const PhotoDetailsState());
   
   Future<void> getPhoto({required int id}) async{
