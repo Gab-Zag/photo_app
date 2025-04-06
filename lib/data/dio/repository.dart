6c1acc19fcbfb3b05f76722e583d8804e29241b6
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2025-04-06 16:06:19 -0300

    Refactor fetchPhotos function to use a single Dio instance and add pagination parameters

--- lib/data/dio/repository.dart
+++ lib/data/dio/repository.dart
@@ -1,11 +1,16 @@
 import 'package:dio/dio.dart';
 import '../../domain/entities/photo.dart';
 
-Future<List<Photo>> fetchPhotos() async {
-  final dio = Dio();
+final Dio _dio = Dio();
 
-  Response response = await dio.get(
+Future<List<Photo>> fetchPhotos({int page = 1, int limit = 0}) async {
+
+  Response response = await _dio.get(
     'https://jsonplaceholder.typicode.com/photos',
+    queryParameters: {
+      '_page': page, 
+      '_limit': limit
+    },
   );
 
   return (response.data as List).map((photo) {
@@ -16,7 +21,9 @@ Future<List<Photo>> fetchPhotos() async {
 Future<Photo> fetchSinglePhoto({required int id}) async {
   final dio = Dio();
 
-  Response response = await dio.get('https://jsonplaceholder.typicode.com/photos');
+  Response response = await dio.get(
+    'https://jsonplaceholder.typicode.com/photos',
+  );
 
   return Photo.fromJson(response.data[id]);
 }
