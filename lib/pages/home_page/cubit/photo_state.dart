a9e6d9776296449f34129419b42d6ca3853c44b7
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2025-04-06 16:06:34 -0300

    Add selectedPhotoIds to PhotoState for managing selected photos

--- lib/pages/home_page/cubit/photo_state.dart
+++ lib/pages/home_page/cubit/photo_state.dart
@@ -5,19 +5,23 @@ import '../../../../domain/entities/photo.dart';
 class PhotoState extends Equatable {
   final bool isLoading;
   final List<Photo> photos;
+  final List<int> selectedPhotoIds;
 
   const PhotoState({
     this.isLoading = false,
     this.photos = const [],
+    this.selectedPhotoIds = const [],
   });
 
   PhotoState copyWith({
     bool? isLoading,
     List<Photo>? photos,
+    List<int>? selectedPhotoIds,
   }) {
     return PhotoState(
       isLoading: isLoading ?? this.isLoading,
       photos: photos ?? this.photos,
+      selectedPhotoIds: selectedPhotoIds ?? this.selectedPhotoIds,
     );
   }
 
@@ -25,5 +29,7 @@ class PhotoState extends Equatable {
   List<Object?> get props => [
         isLoading,
         photos,
+        selectedPhotoIds,
       ];
 }
+
