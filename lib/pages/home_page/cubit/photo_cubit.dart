a6594f411d38f940158ac0fa6439beebdf2b530c
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2025-04-06 18:30:22 -0300

    Refactor PhotoCubit to improve pagination logic and loading state management

--- lib/pages/home_page/cubit/photo_cubit.dart
+++ lib/pages/home_page/cubit/photo_cubit.dart
@@ -1,63 +1,66 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:photo_app/data/dio/repository.dart';
+import '../../../domain/entities/photo.dart';
 import 'photo_state.dart';
 
 class PhotoCubit extends Cubit<PhotoState> {
   PhotoCubit() : super(const PhotoState());
 
   int _currentPage = 1;
-  final int _limit = 15;
+  final int _limit = 10;
   bool _hasMore = true;
-  bool _isLoadingMore = false;
 
-  Future<void> getPhotos({bool loadMore = false}) async {
-    if (_isLoadingMore || (!_hasMore && loadMore)) return;
+  Future<void> getPhotos({bool reset = false}) async {
+    if (state.isLoading || (!_hasMore && !reset)) return;
 
-    if (loadMore) {
-      _isLoadingMore = true;
-      _currentPage++;
-    } else {
+    emit(state.copyWith(isLoading: true));
+
+    if (reset) {
       _currentPage = 1;
       _hasMore = true;
     }
 
-    emit(state.copyWith(isLoading: !loadMore));
-
-    final newPhotos = await fetchPhotos(page: _currentPage, limit: _limit);
-    final allPhotos = loadMore ? [...state.photos, ...newPhotos] : newPhotos;
-
+    final List<Photo> newPhotos = await fetchPhotos(
+      page: _currentPage,
+      limit: _limit,
+    );
+    final List<Photo> updatedPhotos =
+        reset ? newPhotos 
+        : [...state.photos, ...newPhotos];
     _hasMore = newPhotos.length == _limit;
-    _isLoadingMore = false;
 
     emit(state.copyWith(
       isLoading: false,
-      photos: allPhotos,
+      photos: updatedPhotos,
     ));
   }
 
-  void nextPage() {
-    getPhotos(loadMore: true);
-  }
-
-  void previousPage() {
-    if (_currentPage > 1) {
-      _currentPage -= 2;
-      _hasMore = true;
-      getPhotos(loadMore: false);
+  void nextPage(){
+    if(_hasMore){
+      _currentPage++;
+      getPhotos();
     }
   }
 
-  void toggleSelection(int id) {
+  void previousPage(){
+    if(_currentPage > 1){
+      _currentPage--;
+      _hasMore = true;
+      getPhotos(reset: true);
+    }
+  }
+
+  void toggleSelection(int id){
     final selected = Set<int>.from(state.selectedPhotoIds);
-    if (selected.contains(id)) {
+    if(selected.contains(id)){
       selected.remove(id);
-    } else {
+    }else{
       selected.add(id);
     }
     emit(state.copyWith(selectedPhotoIds: selected.toList()));
   }
 
-  void clearSelections() {
+  void clearSelections(){
     emit(state.copyWith(selectedPhotoIds: []));
   }
 }
