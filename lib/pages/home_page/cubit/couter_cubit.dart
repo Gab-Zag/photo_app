4f99bf9df7b668dd4bbbce25bfd6dd046a552a81
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2025-04-06 05:43:32 -0300

    Add CounterCubit for managing counter state with Bloc

---
+++ lib/pages/home_page/cubit/couter_cubit.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'couter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(const CounterState());
+
+  void addCounter(int n) {
+    int counter = state.counter ?? 0;
+    int inputUser;
+    inputUser = counter + n;
+    emit(state.copyWith(counter: inputUser));
+  }
+}
